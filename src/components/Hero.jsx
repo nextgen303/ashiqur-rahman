ddca557b93cea8a0d22411ad272a1500a97b4f3b
ashiq331 <ashiq33030@gmail.com>
2024-07-07 10:50:37 +0600

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,18 +12,18 @@ const Hero = () => {
       data-scroll-speed="-0.4"
       className="pb-28 pt-12 flex flex-col gap-12 w-full -mt-12 -z-10 bg-[#F3F3F5]"
     >
-      <div className="max-w-screen-2xl px-20 max-xl:px-10 max-sm:!px-3 mx-auto">
-        <div className="main flex flex-col items-center justify-center gap-4 pt-20 relative max-sm:items-start">
-          <h4 className="text-[18px]">Hi! There, i’m AshıQur Rahman, </h4>
+      <div className="max-w-screen-2xl px-20 max-xl:px-10 max-sm:!px-3 mx-auto overflow-hidden">
+        <div className="main flex flex-col items-center justify-center gap-4 pt-20 relative">
+          <h4 className="text-[18px] pb-14 max-lg:pb-0">Hi! There, i’m AshıQur Rahman, </h4>
 
-          <h1 className="font-custom text-[120px] text-center max-lg:text-[88px] !max-sm:text-[4vw] max-sm:text-left leading-[80%]">
+          <h1 className="font-custom font-black text-[120px] leading-3 max-lg:text-[88px] max-lg:leading-none text-center max-sm:!text-[54px]">
             Web Designer
           </h1>
-          <h1 className=" text-[120px] text-center leading-[80%] max-lg:text-[88px] !max-sm:text-[4vw] max-sm:text-left  font-black font-custom">
-            & <span className=" text-stroke font-thin">Developer</span>
+          <h1 className="text-[120px]  max-lg:leading-none  text-center max-lg:text-[88px] font-black font-custom max-sm:!text-[54px]">
+            & <span className="text-stroke font-thin">Developer</span>
           </h1>
 
-          <p className="pt-0 pb-3 text-center text-[24px] max-lg:pt-4 max-lg:text-[20px] max-sm:text-left">
+          <p className="pt-0 pb-3 text-center text-[24px] max-lg:pt-4 max-lg:text-[20px">
             I design beautifully simple things and i love what i do. <br />
             Just simple like that!
           </p>
@@ -31,7 +31,7 @@ const Hero = () => {
           <Magnetic>
             <a
               href=""
-              className="bg-[#000000f5] box-shadow-custom text-white w-[140px] px-6 py-2 rounded-full max-md:hidden flex items-center justify-between"
+              className="bg-[#000000f5] box-shadow-custom text-white w-[140px] px-6 py-2 rounded-full  flex items-center justify-between"
             >
               Let’s Talk
               <span className="mt-[2px]">
